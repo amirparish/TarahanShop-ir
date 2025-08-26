f78faa2318830a91084782c88a3e6e350ad60d7e
amirparish <parishamir.ap@gmail.com>
2025-08-26 06:08:38 +0330

    settings, index, base, static

--- tarahanshop/urls.py
+++ tarahanshop/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tarahan.views import home
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home, name='home'),
+] 
+
